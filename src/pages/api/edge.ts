ccd349bd871639cd2041129d6f5af56b8bae8bcf
Dmitry Zorin <dima.zor@mail.ru>
2022-08-11 02:33:35 +0300

    Add an edge function to test it

--- src/pages/api/edge.ts
+++ src/pages/api/edge.ts
@@ -1,5 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
+export const config = {
+	runtime: 'experimental-edge',
+}
+
 export default function handler(
 	request: NextApiRequest,
 	response: NextApiResponse,
