49338dad28be344c0e8af21a02fb7fe9d1657915
Dmitry Zorin <dima.zor@mail.ru>
2022-09-03 16:26:51 +0300

    Update Sidebar.tsx

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,21 +1,26 @@
-import { Box, BoxProps, Flex } from '@chakra-ui/react'
+import { Box, BoxProps, Flex, Theme, useTheme } from '@chakra-ui/react'
 import { Logo } from 'components'
 
 export default function Sidebar({ children, ...props }: BoxProps) {
+	const theme = useTheme<Theme>()
+
 	return (
 		<Box
 			as="nav"
+			display="none"
 			pos="sticky"
 			top={0}
 			h="100vh"
 			flexShrink={0}
-			display={{ base: 'none', md: 'block' }}
 			bg={{ base: 'bg-layer-1', lg: 'none' }}
 			borderRight={{ base: '1px', lg: 0 }}
 			borderColor="border"
 			sx={{
 				'::-webkit-scrollbar': {
 					display: 'none',
+				},
+				[`@media screen and (min-width: ${theme.breakpoints.md})`]: {
+					display: 'block',
 				},
 			}}
 			{...props}
