b356156e50e67d6830be4ae9753ef40c0c5cd41c
Dmitry Zorin <dima.zor@mail.ru>
2022-09-12 14:19:25 +0300

    Fix main menu item hover on mobile

--- src/components/layout/mainMenu/MainMenuItem.tsx
+++ src/components/layout/mainMenu/MainMenuItem.tsx
@@ -5,6 +5,7 @@ import {
 	Text,
 	Tooltip,
 	useBreakpointValue,
+	useMediaQuery,
 } from '@chakra-ui/react'
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
 import { Icon } from 'components'
@@ -28,52 +29,61 @@ export default function MainMenuItem({
 }: MainMenuItemProps) {
 	const { t } = useTranslation()
 	const router = useRouter()
+	const translatedText = t(`layout.menu.items.${text}`)
 	const isActive = new RegExp(`^${href}($|\\${heading ? '?' : 'W'})`).test(
 		router.asPath,
 	)
-	const translatedText = t(`layout.menu.items.${text}`)
 
-	return (
+	const listItem = (
+		<ListItem {...props}>
+			<Link href={href} passHref>
+				<HStack
+					as="a"
+					spacing={3}
+					h={10}
+					borderRadius="lg"
+					outline="none"
+					color={isActive ? 'primary' : 'text-secondary'}
+					px={{ base: 4, lg: 6 }}
+					_hover={{
+						color: 'primary',
+						bg: 'bg-layer-1',
+					}}
+					_focusVisible={{
+						shadow: '0 0 0 2px var(--chakra-colors-primary)',
+					}}
+				>
+					<Icon icon={icon} boxSize="1.125rem" />
+					<Text
+						fontSize="md"
+						display={{
+							base: 'none',
+							lg: 'block',
+						}}
+					>
+						{translatedText}
+					</Text>
+				</HStack>
+			</Link>
+		</ListItem>
+	)
+
+	const [hasCursor] = useMediaQuery('(pointer: fine)')
+	const isTooltipHidden = useBreakpointValue({
+		base: false,
+		lg: true,
+	})
+
+	return hasCursor ? (
 		<Tooltip
 			label={translatedText}
 			placement="right"
 			fontWeight="normal"
-			hidden={useBreakpointValue({
-				base: false,
-				lg: true,
-			})}
+			hidden={isTooltipHidden}
 		>
-			<ListItem {...props}>
-				<Link href={href} passHref>
-					<HStack
-						as="a"
-						spacing={3}
-						h={10}
-						px={{ base: 4, lg: 6 }}
-						borderRadius="lg"
-						color={isActive ? 'primary' : 'text-secondary'}
-						outline="none"
-						_hover={{
-							color: 'primary',
-							bg: 'bg-layer-1',
-						}}
-						_focusVisible={{
-							shadow: '0 0 0 2px var(--chakra-colors-primary)',
-						}}
-					>
-						<Icon icon={icon} boxSize="1.125rem" />
-						<Text
-							fontSize="md"
-							display={{
-								base: 'none',
-								lg: 'block',
-							}}
-						>
-							{translatedText}
-						</Text>
-					</HStack>
-				</Link>
-			</ListItem>
+			{listItem}
 		</Tooltip>
+	) : (
+		listItem
 	)
 }
