5b54b8e7a49d2ac89eadc9b1437da76dc70cbb7e
Dmitry Zorin <dima.zor@mail.ru>
2022-09-26 23:58:07 +0300

    Update app.ts

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,6 +1,4 @@
 export const PER_PAGE = 25
 export const MAX_PER_PAGE = 100
 
-export const DEFAULT_CACHE_VALUE = `public, s-maxage=1, stale-while-revalidate=${
-	30 * 24 * 60 * 60
-}`
+export const DEFAULT_CACHE_VALUE = 'public, s-maxage=1, stale-while-revalidate'
